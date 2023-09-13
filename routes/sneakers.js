6a0e74ff59f669bdb84b9fb6501c9575f9768889
robonexx <robertwagar@gmail.com>
2023-09-13 14:45:05 +0200

    update now works

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const Sneaker = require('../models/SneakerTestModel.js');
 
 // get all sneakers
-// get all articles
 
 router.get('/', async (req, res) => {
   try {
@@ -38,7 +37,31 @@ router.post('/', async (req, res) => {
 });
 
 // delete sneaker
+router.delete('/:id', async (req, res) => {
+  try {
+    const sneaker = await Sneaker.findById(req.params.id);
+    await sneaker.delete();
+    res.status(200).json(`Sneaker with the id ${id}, successfully deleted`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // update sneaker
+
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedSneaker = await Sneaker.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedSneaker);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
