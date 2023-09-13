e7d7edf22e769f9f22b3b9ea87ee22f146c4bc9c
robonexx <robertwagar@gmail.com>
2023-09-13 11:10:03 +0200

    connection to mongo db

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express();
-
 const PORT = process.env.PORT || 8800;
+
+connectDb();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('hello there buddy');
+});
+
+app.get('*', (req, res) => {
+  res.json('404 - not found');
 });
 
 app.listen(PORT, () => {
